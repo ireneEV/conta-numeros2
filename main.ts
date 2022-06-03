2691a0403bca634b0072a48f3dfcf3897d154acf
ireneEV <94906639+ireneEV@users.noreply.github.com>
2022-06-03 12:39:47 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    contador = 1
+    basic.showNumber(contador)
+})
+input.onButtonPressed(Button.AB, function () {
+    contador += 1
+    basic.showNumber(contador)
+})
+input.onButtonPressed(Button.B, function () {
+    contador += 1
+    basic.showNumber(contador)
+})
+let contador = 0
+contador = 0
+basic.showNumber(contador)
